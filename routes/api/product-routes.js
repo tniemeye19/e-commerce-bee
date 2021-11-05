c68682ca2c7542d0bed668ef2a465fd0191ed880
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-11-05 16:22:36 -0400

    removal of unnecessary code'

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { json } = require('sequelize/types');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
